bf21fe517d24bfb5bdb4ae671ed2bae779a0e87a
turlygazhy <erasul_32@mail.ru>
2021-10-13 08:29:05 +0600

    added instruction

---
+++ Halal guide/Modules/Menu/View/InstructionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionViewController.swift
+//  Halal guide
+//
+//  Created by Ерасыл Турлыгажы on 13.10.2021.
+//  Copyright © 2021 Halaldamu. All rights reserved.
+//
+
+import UIKit
+
+class InstructionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Инструкции"
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    //todo add instruction for using map
+}
